29376e4fe8f3b662cb7efeedf3fef85ef94735c6
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2021-10-18 18:23:30 +0200

    means

--- app.R
+++ app.R
@@ -5,10 +5,10 @@ library(tidyverse)
 load("cluster_result.RData")
 
 ui <- dashboardPage(
-  title = "stat2",
-  dashboardHeader(title = "Statisztika II.",
-                  tags$li(a(href = 'https://github.com/MarcellGranat/stat2',
-                            img(src = 'corvinus_logo_HU_rgb.png',
+  title = "Demográfiai klaszterek",
+  dashboardHeader(title = "Demográfia klaszterek",
+                  tags$li(a(href = 'https://github.com/MarcellGranat/demog-cluster',
+                            img(src = 'https://pngimg.com/uploads/github/github_PNG40.png',
                                 title = "Company Home", height = "30px"),
                             style = "padding-top:10px; padding-bottom:10px;"),
                           class = "dropdown")),
@@ -17,7 +17,8 @@ ui <- dashboardPage(
                 value = 3, step = 1),
     sidebarMenu(
       menuItem("Mi ez a projekt?", tabName = "what", icon = icon("question")),
-      menuItem("Térkép", tabName = "sf", icon = icon("question"))
+      menuItem("Térkép", tabName = "sf", icon = icon("question")),
+      menuItem("Átlagok", tabName = "means", icon = icon("question"))
       
     )),
   dashboardBody(
@@ -28,7 +29,14 @@ ui <- dashboardPage(
               )
       ),
       tabItem(tabName = "sf",
-              sliderInput("sf_time", "Év:", min = 1999, max = 2019, value = 2010, animate = TRUE),
+              fluidRow(
+                column(
+                  
+              sliderInput("sf_time", "Év:", min = 1999, max = 2019, value = 2010, 
+                          sep = "", animate = TRUE),
+                width = 4),
+              column(
+                width = 4,
               selectInput("sf_var", "Megjelenít:", choices = c(
                 "Klaszter" = "clust",
                 "Termékenységi arányszám" = "tfr",
@@ -36,35 +44,94 @@ ui <- dashboardPage(
                 "Öregedési index" = "ageing",
                 "Szülőkorban lévő nők aránya" = "motherrate",
                 "Nők foglalkozatási rátája" = "emp"
-              ),
-              box(plotOutput("sf_plot"))
               )
-      )
+              )
+              )
+              ),
+              plotOutput("sf_plot", width = "600px", heigh = "600px")
+      ),
+      tabItem(tabName = "means", plotOutput("mean_plot"))
     )
   )
 )
 
 server <- function(input, output, session) {
   
-  output$sf_plot <- renderPlot({
-    cluster_df %>% 
-      filter(time == input$sf_time, n_cluster == input$n_clust) %>% 
-      left_join(dat) %>% 
-      transmute(data = pmap(list(data, geo, fit), 
-                            function(x, y, z) mutate(x, geo = y, clust = z$cluster))) %>% 
-      pull() %>% 
-      first() %>% 
-      left_join(eurostat::get_eurostat_geospatial(nuts_level = "2")) %>% 
-      mutate(clust = as.factor(clust)) %>% 
-      select(geo, geometry, input$sf_var) %>% 
-      set_names("geo", "geometry", "x") %>% 
-      ggplot() +
-      aes(geometry = geometry, fill = x) + 
-      geom_sf(color = "black")
+  dat_filtered <- reactive({
+    out <- cluster_df %>%
+      filter(n_cluster == input$n_clust) %>%
+      left_join(dat) %>%
+      transmute(time,
+                geo = map(geo, ~ tibble(geo = .)),
+                data = map2(data, geo, bind_cols),
+                data = map2(data, fit, ~ mutate(.x, clust = as.factor(.y$cluster))),
+                gg = map(data, ~ select(., "geo", input$sf_var)),
+                gg = map(gg, set_names, "geo", "var"),
+                gg = map(gg, full_join, eurostat::get_eurostat_geospatial(nuts_level = "2")),
+                gg = map(gg, function(x) {
+                  ggplot(x, aes(geometry = geometry, fill = var)) +
+                    geom_sf(color = "black") + 
+                    scale_x_continuous(limits = c(-10, 40)) + 
+                    scale_y_continuous(limits = c(30, 75)) +
+                    theme_minimal() +
+                    labs(fill = NULL)
+                })
+      )
+    
+    if (input$sf_var != "clust") {
+      gg_limits <- 
+        dat %>% 
+        pull(data) %>% 
+        bind_rows() %>% 
+        pull(input$sf_var) %>% 
+        na.omit() %>% 
+        {c(min(.), max(.))}
+      
+      out <- out %>% 
+        mutate(
+          gg = map(gg, ~ . + scale_fill_viridis_c(limits = gg_limits))
+        )
+    }
+    
+    out
+    
   })
   
+  output$sf_plot <- renderPlot({
+    dat_filtered() %>% 
+      filter(time == input$sf_time) %>% 
+      pull(gg) %>% 
+      first()
+  })
+  
+  output$mean_plot <- renderPlot({
+    cluster_df %>% 
+      filter(n_cluster == input$n_clust) %>% 
+      left_join(dat) %>% 
+      transmute(time, 
+                data = map2(data, geo, ~ mutate(.x, geo = .y)),
+                data = map2(data, fit, ~ mutate(.x, clust = .y$cluster))
+      ) %>% 
+      unnest(data) %>% 
+      group_by(time, clust) %>% 
+      {
+        full_join(
+          summarise_if(., is.numeric, mean, na.rm = T), count(.)
+        )
+      } %>% 
+      ungroup() %>% 
+      pivot_longer(-c(time, clust)) %>% 
+      mutate(clust = as.factor(clust)) %>% 
+      ggplot(aes(time, value, color = clust, fill = clust)) + 
+      geom_line() +
+      geom_point(shape = 21, color = "black") +
+      facet_wrap(~ name, scales = "free_y")
+  })
+
 }
+
+
 
 shinyApp(ui, server)
 
-  
+
